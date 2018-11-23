73198e678c8f65dcf5ad918439453b1a9727fe40
DEMENTOR <dmntr@rambler.ru>
2018-11-24 00:42:59 +1000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+new fullpage('#fullpage', {
+	licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+	sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+	menu:'#menu',
+	scrollBar:false,
+	navigation:true,
+	navigationTooltips:['Секция 1','Секция 2','Секция 3','Секция 4'],
+	slidesNavigation:true,
+	navigationPosition:'left',
+	slidesNavPosition:'top',
+	loopTop:true,
+	loopBottom:true,
+	loopHorizontal:false
+});
